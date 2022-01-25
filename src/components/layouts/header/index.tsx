6a8472310d20a5cfd0ad1d7fd4750ab2c921fc42
MatheusLittig <littig.works@gmail.com>
2022-01-24 22:12:29 -0300

    feat: adding app assets + finishing header

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import * as S from './styles';
 
 export const Header = () => {
   return (
     <S.Wrapper>
-      <h1>Header</h1>
+      <Image src="/logo.svg" width={160} height={50} alt="Bitcasino.io" />
     </S.Wrapper>
   );
 };
