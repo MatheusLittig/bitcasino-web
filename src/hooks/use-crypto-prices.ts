2386fa1b228fe439265fa91a024338b132c9f69c
MatheusLittig <littig.works@gmail.com>
2022-01-26 12:19:35 -0300

    fix: fixing crypto load logic

--- src/hooks/use-crypto-prices.ts
+++ src/hooks/use-crypto-prices.ts
@@ -26,30 +26,40 @@ export const useCryptoPrices = () => {
   ];
 
   useEffect(() => {
-    intialCryptoCurrencies.forEach(async cryptoCurrency => {
-      const fetchedData = await fetchCryptoCurrency(cryptoCurrency.symbol);
-      return setCryptoCurrencies(prevState => [
-        ...prevState,
-        formatCurrency(fetchedData),
-      ]);
-    });
+    if (cryptoCurrencies.length === 0) {
+      intialCryptoCurrencies.forEach(async cryptoCurrency => {
+        const fetchedData = await fetchCryptoCurrency(cryptoCurrency.symbol);
+        return setCryptoCurrencies(prevState => [
+          ...prevState,
+          formatCurrency(fetchedData),
+        ]);
+      });
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function onAddCryptoCurrency(cryptoCurrency: string) {
-    setIsLoading(true);
-    try {
-      const fetchedData = await fetchCryptoCurrency(cryptoCurrency);
+    if (
+      !cryptoCurrencies.find(
+        currency => currency.symbol === cryptoCurrency.toUpperCase(),
+      )
+    ) {
+      setIsLoading(true);
+      try {
+        const fetchedData = await fetchCryptoCurrency(cryptoCurrency);
 
-      setCryptoCurrencies(prevState => [
-        ...prevState,
-        formatCurrency(fetchedData),
-      ]);
+        setCryptoCurrencies(prevState => [
+          ...prevState,
+          formatCurrency(fetchedData),
+        ]);
 
-      return setIsLoading(false);
-    } catch (err) {
-      alert('Invalid crypto currency');
-      return setIsLoading(false);
+        return setIsLoading(false);
+      } catch (err) {
+        alert('Invalid crypto currency');
+        return setIsLoading(false);
+      }
+    } else {
+      return alert('Crypto currency already added');
     }
   }
 
